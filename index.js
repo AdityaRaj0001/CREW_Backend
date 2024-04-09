b77b28bb2782097ade69804006829ed9730c8015
Aditya raj <meadityaraj0001@gmail.com>
2024-04-09 09:11:51 +0530

    email pass change

--- index.js
+++ index.js
@@ -260,13 +260,13 @@ app.post("/sendmail", (req, res) => {
     }
   });
 });
-// transport.verify(function(error, success) {
-//   if (error) {
-//        console.log(error);
-//   } else {
-//        console.log('Server is ready to take our messages');
-//   }
-// });
+transport.verify(function(error, success) {
+  if (error) {
+       console.log(error);
+  } else {
+       console.log('Server is ready to take our messages');
+  }
+});
 
 
 app.listen(port, () => {
